e8b6e5de7e2588bd34dbf661ee9e2657d342b549
MuhammadKhan3 <79418503+MuhammadKhan3@users.noreply.github.com>
2023-02-22 00:35:39 +0500

    pus

--- src/components/dropPane.jsx
+++ src/components/dropPane.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const DropPane = ({floor,appartment, setfloorClicked,floorClicked}) => {
-  return (<div className={`w-full   bg-white h-[25vh] mt-[75vh] z-50   fixed top-0 left-0 hidden  ${appartment.length>0 ? ' sm:block mb:block':''}`} >
+  return (<div className={`w-full   bg-white h-[25vh] mt-[70vh] z-50   fixed top-0 left-0 hidden  ${appartment.length>0 ? ' sm:block mb:block':''}`} >
     <ul className='mt-[2vh] flex flex-col gap-2'>
       <li className='text-primary flex flex-row  text-[20px] justify-between px-4 hover:bg-primary hover:bg-opacity-10'>
         <div className='flex flex-row   justify-between gap-6'>
