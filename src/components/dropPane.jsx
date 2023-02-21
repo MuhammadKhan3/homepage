459291854ea8924a5694737857db3a44285bc2c4
MuhammadKhan3 <79418503+MuhammadKhan3@users.noreply.github.com>
2023-02-22 00:43:45 +0500

    pane

--- src/components/dropPane.jsx
+++ src/components/dropPane.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const DropPane = ({floor,appartment, setfloorClicked,floorClicked}) => {
-  return (<div className={`w-full   bg-white h-[25vh] mt-[70vh] z-50   fixed top-0 left-0 hidden  ${appartment.length>0 ? ' sm:block mb:block':''}`} >
+  return (<div className={`w-full   bg-white h-[25vh] mt-[60vh] z-50   fixed top-0 left-0 hidden  ${appartment.length>0 ? ' sm:block mb:block':''}`} >
     <ul className='mt-[2vh] flex flex-col gap-2'>
-      <li className='text-primary flex flex-row  text-[20px] justify-between px-4 hover:bg-primary hover:bg-opacity-10'>
-        <div className='flex flex-row   justify-between gap-6'>
+      <li className='text-primary flex flex-row  text-[1rem] justify-between px-4 hover:bg-primary hover:bg-opacity-10'>
+        <div className='flex flex-row text-[16px]   justify-between gap-6'>
           <h1>2E</h1>
           <h1>1 Appartment</h1>
         </div>
@@ -12,16 +12,16 @@ const DropPane = ({floor,appartment, setfloorClicked,floorClicked}) => {
           <span>FROM 23.2 MIllION </span>
         </div>
       </li>
-      <li className='text-primary flex flex-row  text-[20px] justify-between px-4'>
+      <li className='text-primary flex flex-row  text-[1rem] justify-between px-4'>
         <div className='flex flex-row   justify-between gap-6'>
-          <h1>2E</h1>
+          <h1 className='text-[0.4rem] '>2E</h1>
           <h1>1 Appartment</h1>
         </div>
         <div>
           <span>FROM 23.2 MIllION </span>
         </div>
       </li>
-      <li className='text-primary flex flex-row  text-[20px] justify-between px-4'>
+      <li className='text-primary flex flex-row  text-[1rem] justify-between px-4'>
         <div className='flex flex-row   justify-between gap-6'>
           <h1>2E</h1>
           <h1>1 Appartment</h1>
